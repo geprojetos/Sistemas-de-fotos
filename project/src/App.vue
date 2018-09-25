3174676adfbf8c120003a70aeed86b1d5b34c49c
Genilson <geprojetosbrasil@gmail.com>
2018-09-25 13:34:26 -0300

    Criando o componente para o menu da aplicação

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <section id="app">
+
+    <app-menu />
     <router-view></router-view>
   </section>
 </template>
 
 <script>
+import Menu from './components/shared/menu/Menu';
+
 export default {
 
+  components: {
+    "app-menu": Menu
+  }
 }
 </script>
 
