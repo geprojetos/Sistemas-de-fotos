9104cae96bd205fe3e46987887451851c0fdb64f
Genilson <geprojetosbrasil@gmail.com>
2018-09-25 14:01:23 -0300

    Implementando a lógica do login

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <section id="app">
 
-    <app-menu />
+    <app-menu :rotas="routes" />
     <router-view></router-view>
   </section>
 </template>
 
 <script>
 import Menu from './components/shared/menu/Menu';
+import { appRouter } from './router.js'
 
 export default {
+
+  data() {
+
+    console.log(appRouter)
+    return {
+      routes: appRouter
+    }
+  },
 
   components: {
     "app-menu": Menu
--- project/src/components/shared/menu/Menu.vue
+++ project/src/components/shared/menu/Menu.vue
@@ -1,11 +1,29 @@
 <template>
-     <h2>Menu</h2>     
+    <header>          
+        <h2>Menu</h2>     
+        <nav>
+            <ul>
+                <li v-for="rota in rotas" :key="rota.id">
+                    <router-link :to="rota.path ? rota.path : '/'">
+                        {{ rota.titulo }}
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script>
-     export default {
-          
-     }
+    export default {
+
+        props: {
+
+            rotas: {
+                required: true,
+                type: Array
+            }
+        }
+    }
 </script>
 
 <style scoped>
