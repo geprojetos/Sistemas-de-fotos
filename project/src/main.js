c33cf68577011e285b5286bfe1592f0e31980b5c
Genilson <geprojetosbrasil@gmail.com>
2018-09-25 16:29:20 -0300

    Adicionando fonts base para a aplicação

--- project/src/main.js
+++ project/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import { appRouter } from './router';
 import './assets/css/reset.css';
+import './assets/css/fonts.css';
 
 Vue.use(VueRouter)
 
