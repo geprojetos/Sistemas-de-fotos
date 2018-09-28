d731efa81662bbd7dcde289a40f6fde8c0259c9d
Genilson <geprojetosbrasil@gmail.com>
2018-09-28 10:48:20 -0300

    Instalando o vue-resource no projeto

--- project/src/main.js
+++ project/src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource';
 import { appRouter } from './router';
 import './assets/css/reset.css';
 import './assets/css/fonts.css';
 import './assets/css/base.css';
 
-Vue.use(VueRouter)
+Vue.use(VueResource);
+Vue.http.options.root = 'http://localhost:3000/';
+Vue.use(VueRouter);
 
 const myRoutes = new VueRouter({
   routes: appRouter,
