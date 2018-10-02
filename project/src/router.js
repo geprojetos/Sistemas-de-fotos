4bb959caa1731bb8ec95696dc9640121c068ab73
Genilson <geprojetosbrasil@gmail.com>
2018-10-02 14:57:24 -0300

    Criando o componente de cadastro e utlizando o lazy loading

--- project/src/router.js
+++ project/src/router.js
@@ -1,11 +1,18 @@
 import Home from './components/page-scope/Home.vue';
+const Cadastro = () => System.import('./components/page-scope/cadastro/Cadastro.vue');
 
 export const appRouter = [
 
-     {
-          path: '',
-          component: Home,
-          name: 'home',
-          titulo: 'Home'
-     }
+    {
+        path: '',
+        component: Home,
+        name: 'home',
+        titulo: 'Home'
+    },
+    {
+        path: '/cadastro',
+        component: Cadastro,
+        name: 'cadastro',
+        titulo: 'Cadastro'
+    }
 ]
