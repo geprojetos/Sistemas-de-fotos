cc4c29b79253d9d2c50d7fad8058d6a0f46140f6
Genilson <geprojetosbrasil@gmail.com>
2018-12-18 20:39:18 -0200

    Adicionando um novo estilo para o menu e o sistema de grid

--- project/src/components/shared/menu/Menu.vue
+++ project/src/components/shared/menu/Menu.vue
@@ -1,20 +1,48 @@
 <template>
     <header class="navbar-dark bg-success">          
         <nav class="navbar container">
-            <router-link class="navbar-brand" to="/"></router-link>
+            <div class="col row">
+                <div class="col-6">
+                    <router-link class="navbar-brand" to="/">
+                        LOGO
+                    </router-link>
+                </div>
 
-            <ul class="navbar-nav">
+                <div class="col-6 text-right">
+                    <span 
+                        class="open-menu menu-cursos-pointer p-2 d-block" 
+                        @click="toggleMenu">
+                        Open
+                    </span>
+                </div>
+            </div>
+
+            <ul 
+                class="navbar-nav pb-2 pt-2"
+                :class="menuActive">
+
+                <li 
+                    class="menu-cursos-pointer text-right mb-4 p-2"
+                    @click="toggleMenu">
+                    Close
+                </li>
+
                 <li 
                     class="nav-item active"
                     v-for="rota in rotas" :key="rota.id">
 
                     <router-link 
-                        class="nav-link"
+                        class="nav-link d-block p-2"
                         :to="rota.path ? rota.path : '/'">
                         {{ rota.titulo }}
                     </router-link>
                 </li>
             </ul>
+
+            <div 
+                :class="bkg"
+                @click="toggleMenu">
+            </div>
         </nav>
     </header>
 </template>
@@ -22,11 +50,39 @@
 <script>
     export default {
 
+        data() {
+            return {
+
+                visible: true,
+                menuActive: 'menu-off',
+                bkg: ''
+            }
+        },
+
         props: {
 
             rotas: {
                 required: true,
                 type: Array
+            }
+        },
+
+        methods: {
+
+            toggleMenu() {
+
+                this.visible = !this.visible; 
+
+                if(this.visible === false) {
+                    
+                    this.menuActive = 'menu-on';
+                    this.bkg = 'menu-active-bkg';
+                } else {
+
+                    this.menuActive = 'menu-off';
+                    this.bkg = '';
+                }
+                
             }
         }
     }
@@ -35,5 +91,4 @@
 <style lang="scss">
 
     @import './menu.scss';
-    
 </style>
